003688cbddede8eac9fe30ed72acbaac1d627223
Maina <mainaedwin913@gmail.com>
2026-01-23 09:31:48 +0300

    updates: routes to use .tsx

--- src/routes/dashboard/index.tsx
+++ src/routes/dashboard/index.tsx
@@ -1,4 +1,4 @@
-import { Index } from '@/pages/dashboard'
+import { Index } from '@/pages/dashboard/Index'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/dashboard/')({
--- src/routes/dashboard/settings.tsx
+++ src/routes/dashboard/settings.tsx
@@ -1,4 +1,4 @@
-import { Settings } from '@/pages/dashboard/settings'
+import { Settings } from '@/pages/dashboard/Settings'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/dashboard/settings')({
